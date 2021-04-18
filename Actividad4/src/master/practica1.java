7349afff50c170430c64f4eb0e356dea99d2acdb
MEMORY SISTEMAS <MEMORY SISTEMAS@MEMORYSISTEMAS>
2021-04-18 15:45:01 +0200

    segundo cambio del codgio practica1

--- Actividad4/src/master/practica1.java
+++ Actividad4/src/master/practica1.java
@@ -3,5 +3,6 @@ package master;
 public class practica1 {
 public static void main(String[] args) {
 	System.out.println("Hola mundo en java git");
+	System.out.println("Segundo hola mundo en java git");
 }
 }
