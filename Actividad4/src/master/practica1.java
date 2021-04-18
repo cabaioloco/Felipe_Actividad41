8f1d5d11e6f7b5a1ace46f89f11512c4ea3dccd3
MEMORY SISTEMAS <MEMORY SISTEMAS@MEMORYSISTEMAS>
2021-04-18 15:45:27 +0200

    tercer cambio del codigo de la practica 1

--- Actividad4/src/master/practica1.java
+++ Actividad4/src/master/practica1.java
@@ -4,5 +4,6 @@ public class practica1 {
 public static void main(String[] args) {
 	System.out.println("Hola mundo en java git");
 	System.out.println("Segundo hola mundo en java git");
+	System.out.println("fin del proyecto");
 }
 }
